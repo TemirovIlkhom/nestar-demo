21762d3aa503a8c5c040fa887a9a85b9fc929505
temirovilkhom <ilhomtemirov9601@gmail.com>
2024-09-29 18:45:34 +0900

    feat: develop

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import withLayoutMain from "@/libs/components/layout/LayoutHome";
+import { NextPage } from "next";
+import { Stack } from "@mui/material";
+import TrendProperties from "@/libs/components/homepage/TrendProperties";
+import PopularProperties from "@/libs/components/homepage/PopularProperties";
+import Advertisement from "@/libs/components/homepage/Advertisement";
+import TopProperties from "@/libs/components/homepage/TopProperties";
+import TopAgents from "@/libs/components/homepage/TopAgents";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+
+const Home: NextPage = () => {
+  return (
+    <Stack className={"home-page"}>
+      <TrendProperties />
+      <PopularProperties />
+      <Advertisement />
+      <TopProperties />
+      <TopAgents />
+    </Stack>
+  );
+};
+
+export default withLayoutMain(Home);
